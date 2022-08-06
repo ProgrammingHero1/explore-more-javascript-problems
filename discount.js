4af59e1e07d5b4346c2441065838d8fdaf3159c7
Programming <programming.hero1@gmail.com>
2022-08-05 23:44:22 -0500

    calculate multi-stage discount

---
+++ discount.js
@@ -0,0 +1,40 @@
+/* 
+1. if ticket numbers is less than 100, per ticket price: 100
+2. if ticket numbers is more than 100, but less than 200. First 100 tickets will be 100/ticket. 
+rest tickets will be 90 taka per piece
+    first 100 ---->100tk
+    rest ----> 90tk
+3. if you purchase more than 200 tickets
+    first 100 ---> 100tk
+    101-200 ---> 90tk
+    200+ ----> 70tk
+*/
+
+function ticketPrice(ticketQuantity){
+    const first100Rate = 100;
+    const second100Rate = 90;
+    const restTicketRate = 70;
+    if(ticketQuantity <= 100 ){
+        const price = ticketQuantity * first100Rate;
+        return price;
+    }
+    else if(ticketQuantity <= 200){
+        const first100Price = 100 * first100Rate;
+        const restTicketQuantity = ticketQuantity - 100;
+        const restTicketPrice = restTicketQuantity * second100Rate;
+        const totalPrice = first100Price + restTicketPrice;
+        return totalPrice;
+    }
+    else{
+        const first100Price = 100 * first100Rate;
+        const second100Price = 100 * second100Rate;
+        const restTicketQuantity = ticketQuantity - 200;
+        const restTicketPrice = restTicketQuantity * restTicketRate;
+        const totalCost = first100Price + second100Price + restTicketPrice;
+        return totalCost;
+    }
+}
+
+
+const price = ticketPrice(120);
+console.log('price: ', price);
